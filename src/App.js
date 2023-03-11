c4c5b991df614a6be50c2f92aeaedaae9687e432
Sree Varshaa <s.sreevarshaa@gmail.com>
2023-03-11 11:17:43 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,319 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+// eslint-disable-next-line no-unused-vars
+const Portfolio = () => {};
+
+function App() {
+  return (
+    <>
+      <header>
+        <div class="scroll-up-btn">
+          <i class="fas fa-angle-up"></i>
+        </div>
+        <nav class="navbar">
+          <div class="max-width">
+            <div class="logo">
+              <a href="#">
+                sreevarshaa<span>.in</span>
+              </a>
+            </div>
+            <ul class="menu">
+              <li>
+                <a href="#home" class="menu-btn">
+                  <strong>Home</strong>
+                </a>
+              </li>
+              <li>
+                <a href="#about" class="menu-btn">
+                  <strong>About</strong>
+                </a>
+              </li>
+              <li>
+                <a href="#services" class="menu-btn">
+                  <strong>Services</strong>
+                </a>
+              </li>
+              <li>
+                <a href="#skills" class="menu-btn">
+                  <strong>Skills</strong>
+                </a>
+              </li>
+
+              <li>
+                <a href="#contact" class="menu-btn">
+                  <strong>Contact</strong>
+                </a>
+              </li>
+            </ul>
+            <div class="menu-btn">
+              <i class="fas fa-bars"></i>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section class="home" id="home">
+          <div class="max-width">
+            <div class="home-content">
+              <div class="text-1">Hello, This is</div>
+              <div class="text-2">Sree Varshaa</div>
+              <div class="text-3">
+                And I'm a <span class="typing">Web Developer</span>
+              </div>
+              <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNstnlbRlfgXdMwrdjHLNvKfPqtxTpfNmNcSfcLpwCpJjPpVDChQWcnwXpppfvVRtPsnhg">
+                Hire Me
+              </a>
+            </div>
+          </div>
+        </section>
+        <section class="about" id="about">
+          <div class="max-width">
+            <h2 class="title">About Me</h2>
+            <div class="about-content">
+              <div class="column left"></div>
+              <image
+                src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                alt="Profile Image 626x626"
+              ></image>
+            </div>
+            <div class="column right">
+              <div class="text">
+                <p>
+                  I'm Sree Varshaa
+                  <p>
+                    Seeking for an entry-level opportunity in an well esteemed
+                    organization where i can express my skills and gather more
+                    knowledege from the field of work.I'm a quick learner and i
+                    have keen interest to learn and master new technologies.
+                  </p>
+                </p>
+                <br />
+                <div class="text">Why Work With Me</div>
+                <p>
+                  I'm a great communicator & love to invest the necessary time
+                  to understand the client's problem very well.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="services" id="services">
+          <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+              <div class="card">
+                <div class="box">
+                  <i class="fas fa-laptop-code"></i>
+                  <div class="text">Web Developer</div>
+                  <p>
+                    I'm a react web developer and I'm well versed in HTML, CSS,
+                    JAVASCRIPT, and REACT JS and well versed in SQL. I'm still
+                    in learning progress on Express JS,Mongo DB, NODE JS,
+                    PREGRESQL and SPRING. I'm persuing B.E Computer Science in
+                    Hindusthan College of Engineering & Technology
+                  </p>
+                </div>
+              </div>
+              <div class="card">
+                <div class="box">
+                  <i class="fas fa-user-secret"></i>
+                  <div class="text">UI Designer</div>
+                  <p>
+                    I'm an UI/UX designer and i am excelled in softwares like
+                    Photoshop,Illustrator,Indesign, AdobeXD and Figma. I am
+                    persuing B.Sc Multimedia in Image Creative Education
+                  </p>
+                </div>
+              </div>
+              <div class="card">
+                <div class="box">
+                  <i class="fas fa-code"></i>
+                  <i class="fas fa-code"></i>
+                  <div class="text">Designing Knowledge</div>
+                  <p>
+                    I'm well versed in designing hence the overall style of the
+                    app,including color schemes,font selection and the types of
+                    buttons and the style preferences for the overall theme.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="skills" id="skills">
+          <div class="max-width">
+            <h2 class="title">My Skills</h2>
+            <div class="skills-content">
+              <div class="column left">
+                <div class="text">INTERNSHIP</div>
+                <p>
+                  <li class="intern">Pumo Technovation "Web Developer"</li>
+                </p>
+                <br />
+                <div class="text">PROJECT</div>
+                <p>
+                  <li class="project">
+                    Portfolio (HTML, CSS, js, Bootstraps & ReactJS)
+                  </li>
+                </p>
+                <br />
+                <div class="text">DEVELOPMENT SKILLS</div>
+                <p>
+                  I'm familiar & I work with HTML,CSS,JavaScript,Bootstrap,React
+                  Js and SQL and I'm still learning more technologies
+                </p>
+                <div class="column right">
+                  <div class="bars">
+                    <div class="info">
+                      <span>Python</span>
+                      <span>90%</span>
+                    </div>
+                    <div class="line python"></div>
+                  </div>
+                  <div class="bars">
+                    <div class="info">
+                      <span>HTML</span>
+                      <span>80%</span>
+                    </div>
+                    <div class="line html"></div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>CSS</span>
+                        <span>80%</span>
+                      </div>
+                      <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>JavaScript</span>
+                        <span>60%</span>
+                      </div>
+                      <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>Bootstraps</span>
+                        <span>80%</span>
+                      </div>
+                      <div class="line bootstraps"></div>
+                    </div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>React JS</span>
+                        <span>60%</span>
+                      </div>
+                      <div class="line reactjs"></div>
+                    </div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>SQL</span>
+                        <span>80%</span>
+                      </div>
+                      <div class="line bootstraps"></div>
+                    </div>
+
+                    <div class="bars">
+                      <div class="info">
+                        <span>Illustrator</span>
+                        <span>80%</span>
+                      </div>
+                      <div class="line illustrator"></div>
+                    </div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>InDesign</span>
+                        <span>80%</span>
+                      </div>
+                      <div class="line indesign"></div>
+                    </div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>Adobe XD</span>
+                        <span>80%</span>
+                      </div>
+                      <div class="line adobexd"></div>
+                    </div>
+                    <div class="bars">
+                      <div class="info">
+                        <span>Figma</span>
+                        <span>80%</span>
+                      </div>
+                      <div class="bars">
+                        <div class="line figma"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="contact" id="contact">
+          <div class="max-width">
+            <h2 class="title">Contact Me</h2>
+            <div class="contact-content">
+              <div class="column-left">
+                <div class="text">Get In Touch</div>
+              </div>
+              <p>
+                Feel free to get in touch with me. I am always open to
+                discussing new projects, creative ideas or opportunities to be a
+                part of your visions. Email me with any questions or inquiries.
+                I'll happy to answer your questions and set up a meeting with
+                you.
+              </p>
+              <div class="icons"></div>
+              <a href="https://instagram.com/_vrshxx_">
+                <div class="info">
+                  <div class="row">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <div class="info">
+                      <div class="head">Address</div>
+                      <div class="sub-title">
+                        &nbsp;Ramanathapuram,Coimbatore{" "}
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <i class="fa-solid fa-envelope"></i>
+                    <div class="row">
+                      <i class="bi bi-translate"></i>
+                      <div class="info">
+                        <div class="head">Language Known</div>
+                        <div class="sub-title">English & Tamil</div>
+                      </div>
+                    </div>
+                    <br />
+                    <div class="social-menu">
+                      <ul>
+                        <li>
+                          <a href="https://www.linkedin.com/in/sree-varshaa-s-00b939255/">
+                            <i class="fa-brands fa-linkedin-in"> Linkedin</i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.instagram.com/_vrshxx_/">
+                            <i class="fa-brands fa-instagram"> Instagram</i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <span>
+          <a href="https://instagram.com/_vrshxx_"> Sree Varshaa</a>
+          <span class="far fa-copyright"></span> 2023 All Rights Reserved.
+          Privacy Policy
+        </span>
+      </footer>
+    </>
+  );
+}
+export default App;
